5774289f9e7451f3e31c6490f799be89fb2e92a9
dion-choy <dionchoynj@gmail.com>
2025-07-21 22:37:04 +0800

    update bucket url to be an local variable

--- app/src/main/java/com/sp/madproj/Utils/Storage.java
+++ app/src/main/java/com/sp/madproj/Utils/Storage.java
@@ -27,7 +27,7 @@ import java.util.Map;
 import uk.me.hardill.volley.multipart.MultipartRequest;
 
 public class Storage {
-    public final static String baseUrl = "https://upevuilypqhjisraltzb.supabase.co/storage/v1/object/images/";
+    public final static String baseUrl = BuildConfig.BUCKET_URL;
     public final static String pfpStorage = baseUrl + "pfp/";
     public final static String identifStorage = baseUrl + "identification/";
     public final static String chatroomIconStorage = baseUrl + "chatroom/icons/";
